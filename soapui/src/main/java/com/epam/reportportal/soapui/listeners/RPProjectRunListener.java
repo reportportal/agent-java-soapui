6f95d4832755a49ead5c0e1d04ec91fce444bd01
o_balcilar <obalcilar@thy.com>
2022-02-17 14:22:29 +0300

    update RPProjectRunListener for log4j2 compatibility.

--- soapui/src/main/java/com/epam/reportportal/soapui/listeners/RPProjectRunListener.java
+++ soapui/src/main/java/com/epam/reportportal/soapui/listeners/RPProjectRunListener.java
@@ -16,12 +16,15 @@
 package com.epam.reportportal.soapui.listeners;
 
 import com.epam.reportportal.soapui.service.SoapUIService;
-import com.epam.ta.reportportal.log4j.appender.ReportPortalAppender;
+import com.epam.ta.reportportal.log4j.appender.ReportPortalLog4j2Appender;
 import com.eviware.soapui.SoapUI;
 import com.eviware.soapui.model.testsuite.*;
-import org.apache.log4j.SimpleLayout;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 
-import java.util.Enumeration;
+import java.util.Iterator;
 
 /**
  * Report portal related implementation of {@link ProjectRunListener}. This
@@ -32,70 +35,71 @@ import java.util.Enumeration;
  */
 public class RPProjectRunListener implements ProjectRunListener {
 
-	public static final String APPENDER_NAME = "ReportPortalAppender";
-	public static final String BASE_APPENDER_NAME = "REPORTPORTAL";
-	public static final String RP_SERVICE = "rp_service";
+    public static final String APPENDER_NAME = "ReportPortalAppender";
+    public static final String BASE_APPENDER_NAME = "REPORTPORTAL";
+    public static final String RP_SERVICE = "rp_service";
 
-	private SoapUIService service;
+    private SoapUIService service;
 
-	@Override
-	public void beforeRun(ProjectRunner runner, ProjectRunContext context) {
-		try {
-			service = RpServiceBuilder.build(context.getProject());
-			defineLogger();
-		} catch (Throwable t) {
-			SoapUI.log("ReportPortal plugin cannot be initialized. " + t.getMessage());
-			service = SoapUIService.NOP_SERVICE;
-		}
-		service.startLaunch();
-		context.setProperty(RP_SERVICE, service);
-	}
+    @Override
+    public void beforeRun(ProjectRunner runner, ProjectRunContext context) {
+        try {
+            service = RpServiceBuilder.build(context.getProject());
+            defineLogger();
+        } catch (Throwable t) {
+            SoapUI.log("ReportPortal plugin cannot be initialized. " + t.getMessage());
+            service = SoapUIService.NOP_SERVICE;
+        }
+        service.startLaunch();
+        context.setProperty(RP_SERVICE, service);
+    }
 
-	@Override
-	public void afterRun(ProjectRunner paramProjectRunner, ProjectRunContext paramProjectRunContext) {
-		service.finishLaunch();
-	}
+    @Override
+    public void afterRun(ProjectRunner paramProjectRunner, ProjectRunContext paramProjectRunContext) {
+        service.finishLaunch();
+    }
 
-	@Override
-	public void beforeTestSuite(ProjectRunner paramProjectRunner, ProjectRunContext paramProjectRunContext, TestSuite paramTestSuite) {
-		service.startTestSuite(paramTestSuite);
-	}
+    @Override
+    public void beforeTestSuite(ProjectRunner paramProjectRunner, ProjectRunContext paramProjectRunContext, TestSuite paramTestSuite) {
+        service.startTestSuite(paramTestSuite);
+    }
 
-	@Override
-	public void afterTestSuite(ProjectRunner paramProjectRunner, ProjectRunContext paramProjectRunContext,
-			TestSuiteRunner paramTestSuiteRunner) {
-		service.finishTestSuite(paramTestSuiteRunner);
-	}
+    @Override
+    public void afterTestSuite(ProjectRunner paramProjectRunner, ProjectRunContext paramProjectRunContext,
+                               TestSuiteRunner paramTestSuiteRunner) {
+        service.finishTestSuite(paramTestSuiteRunner);
+    }
 
-	/**
-	 * Instantiate, initialize report portal log4j appender and add it to all
-	 * groovy.log logger.
-	 */
-	private void defineLogger() {
-		@SuppressWarnings("rawtypes")
+    /**
+     * Instantiate, initialize report portal log4j appender and add it to all
+     * groovy.log logger.
+     */
+    private void defineLogger() {
+        @SuppressWarnings("rawtypes")
 
-		ReportPortalAppender soapUIAppender = new ReportPortalAppender();
-		soapUIAppender.setName(APPENDER_NAME);
-		soapUIAppender.setLayout(new SimpleLayout());
-		Enumeration loggers = org.apache.log4j.Logger.getRootLogger().getLoggerRepository().getCurrentLoggers();
+        ReportPortalLog4j2Appender reportPortalLog4j2Appender = ReportPortalLog4j2Appender.createAppender(APPENDER_NAME, null,
+                PatternLayout.createDefaultLayout());
 
-		while (loggers.hasMoreElements()) {
-			org.apache.log4j.Logger logger = (org.apache.log4j.Logger) loggers.nextElement();
-			if (logger.getAppender(BASE_APPENDER_NAME) != null) {
-				/*
-				 * Report portal soapui log4j appender compatible only with
-				 * groovy.log appender because this logger used for logging user
-				 * logs from groovy scripts. Using soapui log4j appender with
-				 * other appender unsafe because they may be not synchronized
-				 * with listener(logs can be logged to report portal only if
-				 * appender started test step)
-				 */
-				logger.removeAppender(BASE_APPENDER_NAME);
+        Iterator<Logger> loggers = LoggerContext.getContext().getLoggers().iterator();
 
-				if (logger.getName().equals("groovy.log")) {
-					logger.addAppender(soapUIAppender);
-				}
-			}
-		}
-	}
+        while (loggers.hasNext()) {
+            Logger logger = loggers.next();
+            Appender appender = logger.getAppenders().get(BASE_APPENDER_NAME);
+            if (appender != null) {
+                /*
+                 * Report portal soapui log4j appender compatible only with
+                 * groovy.log appender because this logger used for logging user
+                 * logs from groovy scripts. Using soapui log4j appender with
+                 * other appender unsafe because they may be not synchronized
+                 * with listener(logs can be logged to report portal only if
+                 * appender started test step)
+                 */
+                logger.removeAppender(appender);
+
+                if (logger.getName().equals("groovy.log")) {
+                    logger.addAppender(reportPortalLog4j2Appender);
+                }
+            }
+        }
+    }
 }
